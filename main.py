e87dfc61b6f8d67ad2737f4ca972c04b2c4334d6
Darius <dariuszdroba@gmail.com>
2024-01-10 19:47:13 +0200

    ne-amblocat

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ from whoosh import index
 from whoosh.fields import TEXT, Schema
 from whoosh.qparser import QueryParser
 import nltk
+from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download("punkt")
 nltk.download("stopwords")
+
+
+ps = PorterStemmer()
 stop_words = set(stopwords.words("english"))
 # Set up the Whoosh index schema
 schema = Schema(
     title=TEXT(stored=True),
     content=TEXT,
 )
-
-index_path = "wikipedia_index"
-
+index_path = "wikipedia_index2"
 if not os.path.exists(index_path):
     os.makedirs(index_path)
+documentsPath = "C:\\Users\\Darius\\Downloads\\Wiki\\wiki-subset-20140602.tar2"
 
-# Open or create the Whoosh index
 ix = index.create_in(index_path, schema)
-
 # Function to extract title and content from each document
 def extract_title_and_content(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
@@ -40,28 +41,47 @@ def extract_title_and_content(file_path):
 
     for cont in contents:
         c = nltk.word_tokenize(cont)
-
-        c = [word for word in c if (word.lower() not in stop_words) or "==" in word]
-
+        c = [ps.stem(word) for word in c if ((word.lower() not in stop_words) and (word.isalnum()))]
+        words.append(c)
 
     return titles, words
 
 # Index each document
-with ix.writer() as writer:
-    #for file_name in os.listdir("enwiki-20140602-pages-articles.xml-0567.txt"):
-        file_path = "enwiki-20140602-pages-articles.xml-0567.txt"
-
-        title, content = extract_title_and_content(file_path)
-
-        if title and content:
-            # Add a document to the Whoosh index
-            writer.add_document(title=title, content=content)
+def create_index():
+    ix = index.create_in(index_path, schema)
+    with ix.writer() as writer:
+        for file_name in os.listdir(documentsPath):
+            title, content = extract_title_and_content(documentsPath+"\\"+file_name)
+            print(file_name)
+            if title and content:
+                for i in range(len(content)):
+                    cnt = ' '.join(content[i])
+                    writer.add_document(title=title[i], content=cnt)
 
 # Searching the index
-with ix.searcher() as searcher:
-    # Example query: search for documents containing the word "python"
-    query = QueryParser("content", ix.schema).parse("python")
-    results = searcher.search(query)
+def search_index():
+    ix = index.open_dir(index_path)
+    with ix.searcher() as searcher:
+        # Example query: search for documents containing the word "python"
+        query = QueryParser("title", ix.schema).parse("1984")
+        results = searcher.search(query)
 
-    for result in results:
-        print("Title:", result["title"])
+        for result in results:
+            print("Title:", result["title"])
+        reader = searcher.reader()
+        for docnum in range(reader.doc_count_all()):
+            # Get the document
+            doc = reader[docnum]
+
+            # Access the "content" field
+            content = doc.get("content", "")
+
+            # Print the words from the "content" field
+            words = content.split()  # Split the content into words
+            print(f"Words in Document {docnum + 1}: {words}")
+
+a = False
+if(a):
+    create_index()
+else:
+    search_index()
